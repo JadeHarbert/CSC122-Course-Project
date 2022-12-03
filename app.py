e22dfd9dabecaf022ba95b29d7f2b8c5f6046554
Jade Harbert <harbertjade2@gmail.com>
2022-12-03 00:55:41 -0500

    Edited app.py in order to fix development working

--- app.py
+++ app.py
@@ -22,19 +22,22 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # WEBSITE_HOSTNAME exists only in production environment
-if not 'WEBSITE_HOSTNAME' in os.environ:
+if 'WEBSITE_HOSTNAME' not in os.environ:
     # local development, where we'll use environment variables
-    print("Loading config.development and environment variables from .env file.")
-    app.config.from_object('azureproject.development')
+    dbname = 'final_project'
+    dbhost = 'localhost'
+    dbuser = 'postgres'
+    dbpass = 'michael2001'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' + dbname
 else:
     # production
     print("Loading config.production.")
     app.config.from_object('azureproject.production')
-
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-)
+#
+# app.config.update(
+#     SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+#     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+# )
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
